fea07741e5c88acec680461f1764160563e353a1
DESKTOP-VHF2J7B\Lawsford <lawliet-lawsford@hotmail.com>
2019-05-13 15:23:52 +0800

    Use lazy image to load img

--- js/custom.js
+++ js/custom.js
@@ -1768,4 +1768,26 @@ jQuery(document).ready(function() {
 		};
 
 	})( jQuery, window, document );
+	lazyImageLoad();
 });
+
+function lazyImageLoad(){
+	const targets = document.querySelectorAll('img');
+	const lazyLoad = target => {
+		const io = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const img = entry.target;
+				const src = img.getAttribute('data-lazy');
+				img.setAttribute('src', src);
+
+				observer.disconnect();
+			}
+		});
+	});
+
+	io.observe(target)
+	};
+
+	targets.forEach(lazyLoad);
+}
